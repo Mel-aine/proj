4866f7c6987a88e7ed5ef043e74150ad5cdc82b2
Mel-aine <melaineevans7@gmail.com>
2024-11-04 17:42:56 +0100

    nouveau

--- orm/db.js
+++ orm/db.js
@@ -1,8 +1,9 @@
 const {Sequelize} = require('sequelize');
 
-
-let sequelize = new Sequelize('restau_new', 'root' ,  '' , {dialect:'mysql',host:'localhost'})
-
-
+const sequelize = new Sequelize('restaurant', 'root', 'chalecbi_root_admin', {
+    host: '194.163.151.217',
+    port: 3308,
+    dialect: 'mysql',
+  });
 
 module.exports = sequelize
